83a64353cf02a292685a93b64b5949d5a3e3753a
Romain Tello <romain.tello@gmail.com>
2020-01-15 15:36:31 +0100

    Change title

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const buttonData = [
 ];
 
 export const categoryData = [
-  { name: 'Meilleurs ventes', picture: 'https://i.picsum.photos/id/445/600/600.jpg' },
+  { name: 'Meilleures ventes', picture: 'https://i.picsum.photos/id/445/600/600.jpg' },
   { name: 'Offres spéciales', picture: 'https://i.picsum.photos/id/448/600/600.jpg' },
   { name: 'Fins de série', picture: 'https://i.picsum.photos/id/304/600/600.jpg' },
   { name: 'Nouveautés', picture: 'https://i.picsum.photos/id/501/600/600.jpg' },
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavButton, Navbar, Button, Category, CategoriesContainer } from '../components';
+import { Navbar, CategoriesContainer } from '../components';
 
 import '../styles/HomePage.css';
 
@@ -7,9 +7,9 @@ import '../styles/HomePage.css';
 export default () =>
   <div>
     <Navbar />
-    <h1 className="HomePage-title">Bon gros titre</h1>
+    <h1 className="HomePage-title">Avec un nouveau titre</h1>
     <p className="HomePage-description">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim non placeat nihil molestias nisi nesciunt similique facilis sapiente, veritatis numquam eius libero praesentium quaerat, provident nulla iusto earum quisquam distinctio ut repudiandae ipsam eum quidem reiciendis fugiat? Explicabo repudiandae error eos eligendi accusantium, totam fuga ab magni illo quaerat velit dolorem vero dolor maiores consequuntur veniam nisi, provident tempora? Nostrum minima laudantium incidunt laborum itaque natus reiciendis corporis sequi expedita iure molestiae necessitatibus, ipsa praesentium, libero totam facere deserunt vel dicta! Voluptatum, doloremque. Voluptate deleniti qui architecto fugit corporis odit alias officiis dolorum impedit perferendis deserunt, voluptatem debitis in! Recusandae.
+      <code>Lorem ipsum,</code> dolor sit amet consectetur adipisicing elit. Enim non placeat nihil molestias nisi nesciunt similique facilis sapiente, veritatis numquam eius libero praesentium quaerat, provident nulla iusto earum quisquam distinctio ut repudiandae ipsam eum quidem reiciendis fugiat? Explicabo repudiandae error eos eligendi accusantium, totam fuga ab magni illo quaerat velit dolorem vero dolor maiores consequuntur veniam nisi, provident tempora? Nostrum minima laudantium incidunt laborum itaque natus reiciendis corporis sequi expedita iure molestiae necessitatibus, ipsa praesentium, libero totam facere deserunt vel dicta! Voluptatum, doloremque. Voluptate deleniti qui architecto fugit corporis odit alias officiis dolorum impedit perferendis deserunt, voluptatem debitis in! Recusandae.
     </p>
     <CategoriesContainer />
   </div>
