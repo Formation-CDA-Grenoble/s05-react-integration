42f94f04fcca52592077e022318c6a6d7600b3a5
Romain Tello <romain.tello@gmail.com>
2020-01-15 11:09:49 +0100

    Add NavButton component

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as CategoriesContainer } from './CategoriesContainer';
+export { default as Category } from './Category';
+export { default as Navbar } from './Navbar';
+export { default as NavButton } from './NavButton';
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavButton from '../components/NavButton';
+
+export default () =>
+  <div>
+    <NavButton>
+      Pouet
+    </NavButton>
+  </div>
+;
